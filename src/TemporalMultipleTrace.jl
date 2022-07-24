20b9b5b45031e0340e0647d7f7ab294fd44581eb
itsdfish <itsdfish@gmail.com>
2022-07-24 09:57:45 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.28

---
+++ src/TemporalMultipleTrace.jl
@@ -0,0 +1,5 @@
+module TemporalMultipleTrace
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using TemporalMultipleTrace
+using Test
+
+@testset "TemporalMultipleTrace.jl" begin
+    # Write your tests here.
+end
